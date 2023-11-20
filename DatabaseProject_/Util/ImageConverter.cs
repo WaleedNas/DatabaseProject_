4471db54df0ec14650fd0617291069e46caa7779
MuathZaher <81557758+MuathZahir@users.noreply.github.com>
2023-11-20 14:28:28 +0400

    Fixed viewbook page

---
+++ DatabaseProject_/Util/ImageConverter.cs
@@ -0,0 +1,12 @@
+﻿using DataAccess.Models;
+
+namespace DatabaseProject_.Util
+{
+    public class ImageConverter
+    {
+        public static string BytesToSrc(byte[] bytes)
+        {
+            return $"data:image/Bmp;base64,{Convert.ToBase64String(bytes)}";
+        }
+    }
+}
