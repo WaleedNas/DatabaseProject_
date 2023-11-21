56511098785bd623582dc101dc9015f4e3456101
MuathZaher <81557758+MuathZahir@users.noreply.github.com>
2023-11-21 14:01:31 +0400

    Final

---
+++ DBProjectDatabase/dbo/StoredProcedures/Order/spOrder_GetTotalPrice.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[spOrder_GetTotalPrice]
+	@OrderId INT
+AS
+	BEGIN
+	SET NOCOUNT ON;
+	
+	SELECT SUM(Quantity * Price) AS TotalPrice
+	FROM OrderItem JOIN Book ON OrderItem.BookId = Book.BookId
+	WHERE OrderId = @OrderId
+END
+RETURN 0
